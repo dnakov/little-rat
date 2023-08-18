e6742d2416a48373a816e10e5c52c4ccf40c0736
daniel nakov <daniel.nakov@gmail.com>
2023-08-18 15:04:22 -0400

    fix build script

--- scripts/build-chrome-store.sh
+++ scripts/build-chrome-store.sh
@@ -6,10 +6,12 @@ mkdir -p $DIST
 if [ ! -z "$VER" ]; then
   jq --arg VER "$VER" '.version = $VER | .optional_permissions = [ "declarativeNetRequestFeedback", "management" ] | .permissions = .permissions - ["management", "declarativeNetRequestFeedback"]' manifest.json > $DIST/manifest.json
 else
-  cp manifest.json $DIST/manifest.json
+  jq '.optional_permissions = [ "declarativeNetRequestFeedback", "management" ] | .permissions = .permissions - ["management", "declarativeNetRequestFeedback"]' manifest.json > $DIST/manifest.json
 fi
 
 cp -r src $DIST/src
 mkdir -p $DIST/assets
 cp assets/little-rat-* $DIST/assets
-zip little-rat.zip -qr $DIST/*
+pushd $DIST
+zip little-rat.zip -qr ./*
+popd
