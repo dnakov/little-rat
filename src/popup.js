81f75aa29988d027d554e99a5143b48d14b3627d
tofuliang <tofuliang@apifox.com>
2023-11-04 14:48:16 +0800

    feat: block request by url

--- src/popup.js
+++ src/popup.js
@@ -6,31 +6,41 @@ let theme;
 
 async function toggleMuteExt(e) {
   const id = e.target.id.replace('mute-', '');
-  await port.postMessage({ type: 'toggleMute', data: { id } });  
+  await port.postMessage({ type: 'toggleMute', data: { id } });
   await updateList();
 }
 
 async function toggleBlockExt(e) {
   const id = e.target.id.replace('block-', '');
-  await port.postMessage({ type: 'toggleBlock', data: { id } });  
+  await port.postMessage({ type: 'toggleBlock', data: { id } });
+  await updateList();
+}
+
+async function toggleBlockExtUrl(e) {
+  const id = e.target.id.replace('block-ext-url-', '').split(' ');
+  const urlInfo = id[1].split('|');
+  await port.postMessage({ type: 'toggleBlockExtUrl', data: { extId: urlInfo[1], method: id[0], url: urlInfo[0] } });
   await updateList();
 }
 
 async function toggleExt(e) {
   const id = e.target.id.replace('toggle-', '');
-  await port.postMessage({ type: 'toggleExt', data: { id } });  
+  await port.postMessage({ type: 'toggleExt', data: { id } });
   await updateList();
 }
 
 document.addEventListener('DOMContentLoaded', async () => {
   setTheme();
-  if(IS_STORE) {
+  if (IS_STORE) {
     document.body.classList.add('is-store');
     document.getElementById('reset').style.visibility = 'hidden';
-  }  
+  }
   document.getElementById('exts-body').addEventListener('click', (e) => {
-    if(e.target.id.startsWith('mute-')) {
+    if (e.target.id.startsWith('mute-')) {
       toggleMuteExt(e);
+      e.preventDefault();
+    } else if (e.target.id.startsWith('block-ext-url-')) {
+      toggleBlockExtUrl(e);
       e.preventDefault();
     } else if (e.target.id.startsWith('block-')) {
       toggleBlockExt(e);
@@ -40,19 +50,19 @@ document.addEventListener('DOMContentLoaded', async () => {
       e.preventDefault();
     }
   })
-  document.getElementById('reset').addEventListener('click', 
+  document.getElementById('reset').addEventListener('click',
     () => port.postMessage({ type: 'reset' }));
   document.getElementById('theme').addEventListener('click', toggleTheme);
 
-  document.getElementById('request-perm').addEventListener('click', 
+  document.getElementById('request-perm').addEventListener('click',
     async () => {
-      const granted = await chrome.permissions.request({permissions: ['management']})
-      if(granted) {
+      const granted = await chrome.permissions.request({ permissions: ['management'] })
+      if (granted) {
         chrome.runtime.reload();
       }
     });
-  const hasPerm = await chrome.permissions.contains({permissions: ['management']})
-  if(!hasPerm) {
+  const hasPerm = await chrome.permissions.contains({ permissions: ['management'] })
+  if (!hasPerm) {
     document.querySelector('.permissions').style.visibility = 'visible';
   }
 });
@@ -65,10 +75,10 @@ port.onMessage.addListener((message) => {
 
 function setTheme(theme) {
   theme = theme || localStorage.theme;
-  if(!theme) {
-    if(window.matchMedia('(prefers-color-scheme: dark)').matches) {
+  if (!theme) {
+    if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
       theme = 'dark';
-    } else{ 
+    } else {
       theme = 'light';
     }
     localStorage.theme = theme;
